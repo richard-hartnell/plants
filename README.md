911191cfaffe60986e6ed2176dee4e83b17a3b14
richard-hartnell <richard@richardhartnell.com>
2025-01-15 21:23:21 -0800

    Fleshed out todos in README, added subclass bits

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # plants
 a garden scripting station.
 
-
 ## The new version of the PLANTS sheet
 Now with more dynamic behavior. Designed so I can quit Taskade.
 
 ### TODO LIST
 
+- incorporate dates.
 - handle a table of plants and a table of plots.
 - create a garden plan based on companion planting and rotation
 - populate Google Calendar events with:
@@ -25,3 +25,19 @@ Now with more dynamic behavior. Designed so I can quit Taskade.
 - create something like workshop_scheduler to plot plants?
 - add a verison of bcg-emailer for extra harrassment
 - integrate that companion planting sheet where each object can be mutually referred to
+
+### What's it do?
+
+-Reads in database of plants from SQL
+
+-Automatically detects companion planting compatibilities/incompatibilities
+
+-Sorts cold plants, warm plants into companion planting categories (three sisters as alpha test)
+
+-Measures max root length of each companion planting group
+
+-Determines number of plantings of each plant and calculates seeds required
+
+-Schedules plantings for each biweekly planting sesh
+
+-Sends emails
--- plants.py
+++ plants.py
@@ -2,7 +2,7 @@ import datetime
 import json
 
 class Plant:
-    def __init__(self, name, varietal='', root_length, number_per_hex, early, late, maturity_age, sow1_start, sow2_start='', sow1_end, sow2_end=''):
+    def __init__(self, name, root_length, number_per_hex, early, late, maturity_age, sow1_start, sow1_end, direct_sow, sow2_start=None, sow2_end=None, varietal=None):
         self.name = name
         self.varietal = varietal
         self.root_length = root_length
@@ -14,11 +14,43 @@ class Plant:
         self.sow1_end = sow1_end
         self.sow2_start = sow2_start
         self.sow2_end = sow2_end
-        self.season_length = (sow1_end - sow1_start) + (sow2_end - sow2_start)
-
-
-    def water(self):
-        self.last_watered_on = datetime.datetime.now()
+        self.direct_sow = direct_sow
+        self.season_length = (sow1_end - sow1_start).days + (sow2_end - sow2_start).days
 
     def __str__(self):
         return f"{self.name} ({self.species})"
+    
+    def plot(self):
+        # run the comparison of plant's companions
+        pass
+    
+class Bean(Plant):
+    def __init__(self, name, varietal=None):
+        # Default values for beans
+        root_length = 6  # inches
+        number_per_hex = None
+        early = True
+        late = True
+        maturity_age = 60  # days
+        sow1_start = date(2025, 4, 15)  # Spring planting
+        sow1_end = date(2025, 5, 30)
+        sow2_start = date(2025, 7, 15)  # Fall planting
+        sow2_end = date(2025, 8, 15)
+        direct_sow = True
+        
+        super().__init__(
+            name=name,
+            varietal=varietal,
+            root_length=root_length,
+            number_per_hex=number_per_hex,
+            early=early,
+            late=late,
+            maturity_age=maturity_age,
+            sow1_start=sow1_start,
+            sow1_end=sow1_end,
+            sow2_start=sow2_start,
+            sow2_end=sow2_end,
+            direct_sow=direct_sow
+        )
+        self.species = "Phaseolus vulgaris"
+        self.climbing = True  # Bean-specific attribute
