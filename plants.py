cb645fa1ece8ef2dcd96f21fb9b6317729769f3a
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2025-01-16 22:33:48 -0800

    Subclasses expanded (w bugs)

--- plants.py
+++ plants.py
@@ -1,6 +1,11 @@
 import datetime
 import json
 
+year = datetime.now().year
+
+first_frost = datetime.date(year, 10, 22)
+last_frost = datetime.date(year, 4, 15)
+
 class Plant:
     def __init__(self, name, root_length, number_per_hex, early, late, maturity_age, sow1_start, sow1_end, direct_sow, sow2_start=None, sow2_end=None, varietal=None):
         self.name = name
@@ -20,6 +25,17 @@ class Plant:
     def __str__(self):
         return f"{self.name} ({self.species})"
     
+    def when_to_sow(self, early, late):
+        if self.early:       
+            sow1_start = date(year, 2, 1)  # Spring planting
+            sow1_end = date(0000, 5, 1)
+        if self.late:
+            sow2_start = date(year, 7, 15)  # Fall planting
+            sow2_end = date(year, 8, 15)
+        else:
+            sow1_start = date(year,5, 1)
+            sow1_end = date(year, 7, 1)
+
     def plot(self):
         # run the comparison of plant's companions
         pass
@@ -29,14 +45,10 @@ class Bean(Plant):
         # Default values for beans
         root_length = 6  # inches
         number_per_hex = None
-        early = True
-        late = True
+        early = False
+        late = False
         maturity_age = 60  # days
-        sow1_start = date(2025, 4, 15)  # Spring planting
-        sow1_end = date(2025, 5, 30)
-        sow2_start = date(2025, 7, 15)  # Fall planting
-        sow2_end = date(2025, 8, 15)
-        direct_sow = True
+        direct_sow = False
         
         super().__init__(
             name=name,
@@ -46,11 +58,8 @@ class Bean(Plant):
             early=early,
             late=late,
             maturity_age=maturity_age,
-            sow1_start=sow1_start,
-            sow1_end=sow1_end,
-            sow2_start=sow2_start,
-            sow2_end=sow2_end,
             direct_sow=direct_sow
+            self.when_to_sow(early, late)
         )
         self.species = "Phaseolus vulgaris"
         self.climbing = True  # Bean-specific attribute
