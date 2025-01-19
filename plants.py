8334ef939c12c95085cca5764ea5244f3e3e73b7
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2025-01-18 22:40:04 -0800

    Added basic SQLite functionality

--- plants.py
+++ plants.py
@@ -1,26 +1,28 @@
 import datetime
 import json
+import sqlite3
+import pandas as pd
 
-year = datetime.now().year
-
+year = datetime.datetime.now().year
 first_frost = datetime.date(year, 10, 22)
 last_frost = datetime.date(year, 4, 15)
 
+conn = sqlite3.connect('nanofarm.db')
+df = pd.read_sql('SELECT * FROM plants', conn)
+print (df)
+
+# Create a dictionary of plants
+
+
 class Plant:
-    def __init__(self, name, root_length, number_per_hex, early, late, maturity_age, sow1_start, sow1_end, direct_sow, sow2_start=None, sow2_end=None, varietal=None):
+    def __init__(self, name, root_length, number_per_hex, early, late, maturity_age, direct_sow):
         self.name = name
-        self.varietal = varietal
         self.root_length = root_length
         self.early = early
         self.late = late
         self.maturity_age = maturity_age
         self.number_per_hex = number_per_hex
-        self.sow1_start = sow1_start
-        self.sow1_end = sow1_end
-        self.sow2_start = sow2_start
-        self.sow2_end = sow2_end
         self.direct_sow = direct_sow
-        self.season_length = (sow1_end - sow1_start).days + (sow2_end - sow2_start).days
 
     def __str__(self):
         return f"{self.name} ({self.species})"
@@ -40,26 +42,70 @@ class Plant:
         # run the comparison of plant's companions
         pass
     
-class Bean(Plant):
-    def __init__(self, name, varietal=None):
-        # Default values for beans
-        root_length = 6  # inches
-        number_per_hex = None
-        early = False
-        late = False
-        maturity_age = 60  # days
-        direct_sow = False
+# class Bean(Plant):
+#     def __init__(self, name, varietal=None):
+#         # Default values for beans
+#         root_length = 6  # inches
+#         number_per_hex = None
+#         early = False
+#         late = False
+#         maturity_age = 60  # days
+#         direct_sow = False
         
-        super().__init__(
-            name=name,
-            varietal=varietal,
-            root_length=root_length,
-            number_per_hex=number_per_hex,
-            early=early,
-            late=late,
-            maturity_age=maturity_age,
-            direct_sow=direct_sow
-            self.when_to_sow(early, late)
-        )
-        self.species = "Phaseolus vulgaris"
-        self.climbing = True  # Bean-specific attribute
+#         super().__init__(
+#             name=name,
+#             varietal=varietal,
+#             root_length=root_length,
+#             number_per_hex=number_per_hex,
+#             early=early,
+#             late=late,
+#             maturity_age=maturity_age,
+#             direct_sow=direct_sow,
+#             self.when_to_sow(self, early, late)
+#         )
+
+# class Beet(Plant):
+#     def __init__(self, name, varietal=None):
+#         # Default values for beets
+#         root_length = 6  # inches
+#         number_per_hex = 9
+#         early = True
+#         late = False
+#         maturity_age = 60  # days
+#         direct_sow = True
+        
+#         super().__init__(
+#             name=name,
+#             varietal=varietal,
+#             root_length=root_length,
+#             number_per_hex=number_per_hex,
+#             early=early,
+#             late=late,
+#             maturity_age=maturity_age,
+#             direct_sow=direct_sow,
+#             self.when_to_sow(early, late)
+#         )
+
+# class Broccoli(Plant):
+#     def __init__(self, name, varietal=None):
+#         # Default values for broccoli
+#         root_length = 12  # inches
+#         number_per_hex = 1
+#         early = True
+#         late = False
+#         maturity_age = 60  # days
+#         direct_sow = False
+        
+#         super().__init__(
+#             name=name,
+#             varietal=varietal,
+#             root_length=root_length,
+#             number_per_hex=number_per_hex,
+#             early=early,
+#             late=late,
+#             maturity_age=maturity_age,
+#             direct_sow=direct_sow,
+#             self.when_to_sow(early, late)
+#         )
+
+conn.close()
