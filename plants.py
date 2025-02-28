c164560cb386cd31764e0105ba9ffb1ebf7a1fcd
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2025-02-27 20:24:08 -0800

    Added more todos and a this-year updater

--- plants.py
+++ plants.py
@@ -16,13 +16,13 @@ cursor = conn.cursor()
 
 df_plant_types = pd.read_sql('SELECT * FROM plant_types', conn)
 df_plots = pd.read_sql('SELECT * FROM plots', conn)
-df_2025 = pd.read_sql('SELECT * FROM this_year', conn)
+df_this_year = pd.read_sql('SELECT * FROM this_year', conn)
 
-def add_fake_plot():
+def plant_this_year(_id, _varietal, _plant_type, _plot):
     cursor.execute('''
-                INSERT INTO 'Plots' (id, plot_name, area, root_max)
+                INSERT INTO 'this_year' (id, varietal, plant_type, plot)
                 VALUES (?, ?, ?, ?)
-                ''', (6969, 'Test Plot II', 100, 100))
+                ''', (_id, _varietal, _plant_type, _plot))
     conn.commit()
     pass
 
@@ -31,7 +31,7 @@ class Plant:
         pass
 
     def when_to_sow(self):
-        if self.cold:       
+        if self.cold:
             self.sow1_start = date(year, 2, 1)  # Spring planting
             self.sow1_end = date(0000, 5, 1)
         if self.hot:
@@ -62,61 +62,76 @@ def generate_plant_class(name, base_class, attrs):
     return type(name, (base_class,), attrs)
 
 def plot_plants():
+    # # Assign plants to plots
+    #     
     pass
 
-for row in df_plant_types.itertuples():
+def check_compatibility(plant):
+    _friendly_plots = []
+    for plot in plots:
+        for _plant in plot:
+            if _plant.name in plant.friends:
+                print (plot.name)
+        
+    return False
 
-    _plant_attrs = {
-        'plant_type': row.plant_type,
-        'root_distance': row.root_distance,
-        'cold': row.cold,
-        'hot': row.hot,
-        'days_to_mature': row.days_to_mature,
-        'direct_sow': row.direct_sow,
-        'varietal': row.varietal,
-        'friends': row.friends,
-        'foes': row.foes,
-        'allium': row.allium,
-        'brassica': row.brassica,
-    }
+def fetch_plant_types():    
+    for row in df_plant_types.itertuples():
 
-    if row.plant_type in plant_types:
-        pass
-    else:
-        plant_types.append(generate_plant_class(row.plant_type, Plant, _plant_attrs))
+        _plant_attrs = {
+            'plant_type': row.plant_type,
+            'root_distance': row.root_distance,
+            'cold': row.cold,
+            'hot': row.hot,
+            'days_to_mature': row.days_to_mature,
+            'direct_sow': row.direct_sow,
+            'varietal': row.varietal,
+            'friends': row.friends,
+            'foes': row.foes,
+            'allium': row.allium,
+            'brassica': row.brassica,
+        }
 
-for row in df_plots.itertuples():
-    _plot_attrs = {
-        'plot_name': row.plot_name,
-        'area': row.area,
-        'root_max': row.root_max,
-    }
-    if row.plot_name in plots:
-        pass
-    else:
-        plots.append(generate_plant_class(row.plot_name, Plot, _plot_attrs))
+        if row.plant_type in plant_types:
+            pass
+        else:
+            plant_types.append(generate_plant_class(row.plant_type, Plant, _plant_attrs))
 
-for plant in plant_types:
-    print(plant.plant_type)
-
-for plot in plots:
-    print(plot.plot_name)
-
-add_fake_plot()
-#this doesn't re-fetch the plots from the db, so the fake plot isn't printed.
-
-for plot in plots:
-    print(plot.plot_name)
+def fetch_plots():
+    for row in df_plots.itertuples():
+        _plot_attrs = {
+            'plot_name': row.plot_name,
+            'area': row.area,
+            'root_max': row.root_max,
+        }
+        if row.plot_name in plots:
+            pass
+        else:
+            plots.append(generate_plant_class(row.plot_name, Plot, _plot_attrs))
 
 conn.close()
 
 #OO
 # on run, load all plants from the database
 # check for any plants in the db that aren't in a plot.
-# assign them automatically if poss.
+# assign them automatically if possible
 # # use the comparison planting sheet in a function like workshop_scheduler
 # if not, ask user to assign them
 # then, calc max root length for each plot
 # apply calculation from PLANTS sheet for hex size & number of hexes
 # ...
 # make plants
+
+#OO 2
+# # read in plots.
+# read in plant types.
+
+# # # check for companion planting compatibility
+# # start with the big plants.
+# if there's a different big plant type in a plot, move to a different plot.
+# this means you should have a limit of three big hot plants and three (six?) big cold plants.
+# # interplant the smaller plants (root length ~1/2 the big L)
+# if there's a friend in the plot, add 1 point.
+# if there's a foe in the plot, subtract 3 points.
+# # keep in mind cold/hot planting.
+# make cold plants ignore hot plants and vice versa.
